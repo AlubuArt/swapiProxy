cb78fd9b36b87999d4a1e855a361c3d990f727cc
AlubuArt <jc@jcvisueldesign.dk>
2021-04-21 18:58:48 +0200

    init

---
+++ routes/people.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const axios = require('axios');
+const router = express.Router();
+
+
+router.get("/", async (req, res) => {
+
+let param = req;
+let config = {
+  method: 'get',
+  url: `https://swapi.dev/api/people/${param}`,
+}
+
+axios(config)
+.then((response) => {
+  console.log(JSON.stringify(response.data));
+  console.log(response.data)
+  res.send(response.data)
+  
+})
+.catch((error) => {
+  console.log(error);
+});
+
+});
+
+module.exports = router;
+
+
+
+
+
